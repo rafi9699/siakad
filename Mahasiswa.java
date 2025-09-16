d9e9e7544a36aabfa44afcb440efd6c44a9e337c
rafi9699 <rafifirizki01@gmail.com>
2025-09-16 16:50:26 +0700

    Update Mahasiswa.java

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,5 +1,4 @@
 public class Mahasiswa {
-    
     String nama;    
     int NIM;
     String kelas;
@@ -10,14 +9,27 @@ public class Mahasiswa {
         System.out.println("Kelas : " + kelas);
     }
 
-    void mengajar
+    void presensi() {
+        System.out.println(nama + " melakukan presensi.");
+    }
+
+    void krs() {
+        System.out.println(nama + " sedang melakukan pengisian KRS.");
+    }
+
+    void kumpulkanTugas(String mataKuliah) {
+        System.out.println(nama + " mengumpulkan tugas " + mataKuliah + ".");
+    }
 
     public static void main(String[] args) {
         Mahasiswa mhs = new Mahasiswa();
         mhs.nama = "Rafi";
-        mhs.NIM = 2403003;
-        mhs.kelas = "TI-2A";
+        mhs.NIM = 123456;
+        mhs.kelas = "IF-1";
         
         mhs.tampilkanInfo();
+        mhs.presensi();
+        mhs.krs();
+        mhs.kumpulkanTugas("Pemrograman Java");
     }
 }
