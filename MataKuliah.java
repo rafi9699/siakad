b14f210c7e9acdf8f0b60e0b7bb7a46ab4a7eac8
unknown <nailansTI2A>
2025-09-16 14:23:00 +0700

    inisiasi class MataKuliah

---
+++ MataKuliah.java
@@ -0,0 +1,15 @@
+public class MataKuliah {
+
+    public String kodeMataKuliah;
+    public String namaMataKuliah;
+
+    public void menampilkanJumlahSks(){
+        System.out.println("1 SKS Teori, 2 SKS Praktikum");
+    }
+    
+    public String showMataKuliah(){
+        return "Mata Kuliah yang mempelajari paradigma pemograman beriorentasi objek";
+    }
+        
+
+}
